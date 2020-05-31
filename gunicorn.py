8a6186aa9815d5206ca2707b3ff034cc6c970146
MincoX <MincoX>
2020-05-31 23:37:08 +0800

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 # 监听地址和端口
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:5000'
 
 # 进程的数量，缺省为1
 workers = multiprocessing.cpu_count() * 2 + 1
@@ -11,20 +11,20 @@ daemon = 'false'
 
 # worker进程的工作方式
 # 有 sync, eventlet, gevent, tornado, gthread, 缺省值sync
-worker_class = "gevent"
+# worker_class = "gevent"
 
 # 指定项目的根目录，在 apis 加载之前，进入到此目录
-chdir = '/home/project/flask-server'
+chdir = '/usr/src/flaskServer'
 
 # 设置进程文件目录
-pidfile = '/home/project/flask-server/gunicorn.pid'
+pidfile = '/usr/src/flaskServer/gunicorn.pid'
 
 # 客户端最大同时连接数。只适用于eventlet， gevent工作方式
 worker_connections = 1000
 
 # 设置访问日志和错误信息日志路径
-accesslog = '/home/project/flask-server/gunicorn_acess.log'
-errorlog = '/home/project/flask-server/gunicorn_error.log'
+accesslog = '/usr/src/flaskServer/gunicorn_acess.log'
+errorlog = '/usr/src/flaskServer/gunicorn_error.log'
 
 # 设置日志等级
-loglevel = 'warning'
+loglevel = 'info'
